c4ec66a41fd54c97c61da7104f9f331c5bfeb551
chrismbryant <cbryant@berkeley.edu>
2020-02-15 13:41:49 -0800

    Add error animation

--- js/go.js
+++ js/go.js
@@ -6,15 +6,28 @@ function autoSelect() {
 
 function enter() {
   if (event.keyCode == 13) {
-    document.getElementById('go').click()
+    document.getElementById("go").click()
   }            
+}
+
+function showError(s) {
+  var title = document.getElementById("title");
+  $(title)
+    .stop()
+    .animate({color: "red"}, 10)
+    .effect("shake", {times: 1}, 120)
+    .animate({color: "#232F3E"}, 1200);
 }
 
 function getUrl() {
   var input = document.getElementById("search-input").value;
-  var url = ""
-  if (input.length > 4 && input.substring(0, 5) == "s3://") {
-    url = "https://console.aws.amazon.com/s3/buckets/" + input.substring(5,);
-  }
-  window.location.href = url;
+  var url = "";
+  const pieces = input.split("//");
+  if (pieces.length != 2) {
+    showError(input);
+  } else {
+    const suffix = pieces[pieces.length - 1];
+    url = "https://console.aws.amazon.com/s3/buckets/" + suffix;
+    window.location.href = url;
+  } 
 }

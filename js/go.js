3e3bac8ec4650d7d0d20312dcb2dfdca579effbf
Chris Bryant <cbryant@cmb-mbp.local>
2020-02-17 10:39:58 -0800

    Fix shake by placing title in div, and add success color

--- js/go.js
+++ js/go.js
@@ -12,21 +12,31 @@ function enter() {
 
 function showError() {
   var title = document.getElementById("title");
+  var text = document.getElementById("search-input");
   $(title)
     .stop()
     .animate({color: "red"}, 10)
     .effect("shake", {times: 1}, 120)
     .animate({color: "#232F3E"}, 1200);
+  $(text)
+    .stop()
+    .animate({color: "red"}, 10)
+    .animate({color: "#232F3E"}, 1200);
+}
+
+function showSuccess() {
+  document.getElementById("search-input").style.color = "#00B140";
 }
 
 function getUrl() {
   var input = document.getElementById("search-input").value;
-  const pieces = input.split("//");
+  const pieces = input.split("://");
   if (pieces.length != 2) {
     showError();
   } else {
     const suffix = pieces[pieces.length - 1];
     const url = "https://console.aws.amazon.com/s3/buckets/" + suffix;
+    showSuccess();
     window.location.href = url;
   } 
 }
